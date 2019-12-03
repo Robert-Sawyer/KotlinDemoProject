62321399b6b26c594f8c05780fa448fc57494722
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-03 20:40:24 +0100

    Added new file with input lines option.

---
+++ src/Inputs.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+
+    print("Enter name: ")
+    val name:String = readLine()!!.toString()
+    print("Enter age: ")
+    val age = readLine()!!.toInt()
+
+    println("Name: $name")
+    println("Age: $age")
+
+}
